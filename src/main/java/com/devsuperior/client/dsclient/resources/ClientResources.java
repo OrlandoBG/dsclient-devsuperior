4006b3f4ad8b783ad8e774b4471ffd15a1cf5649
OrlandoBG <orlandopsn1@gmail.com>
2022-02-15 17:02:10 -0300

    Paginação e criação da classe ClientDTO

--- src/main/java/com/devsuperior/client/dsclient/resources/ClientResources.java
+++ src/main/java/com/devsuperior/client/dsclient/resources/ClientResources.java
@@ -1,16 +1,38 @@
 package com.devsuperior.client.dsclient.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.client.dsclient.dto.ClientDTO;
+import com.devsuperior.client.dsclient.services.ClientService;
 
 @RestController
 public class ClientResources {
 	
+	@Autowired
+	private ClientService service;
+	
 	@RequestMapping(value = "/clients")
 	
-	public ResponseEntity findAll() {
-		return null;
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			 ){
+		
+		PageRequest pageRequest = PageRequest.of(page,linesPerPage,Direction.valueOf(direction),orderBy);
+		Page<ClientDTO> dto = service.findAllPaged(pageRequest);
+		
+		return ResponseEntity.ok().body(dto);
+		
+		
 	}
 
 }
--- src/main/java/com/devsuperior/client/dsclient/services/ClientService.java
+++ src/main/java/com/devsuperior/client/dsclient/services/ClientService.java
@@ -1,9 +1,29 @@
 package com.devsuperior.client.dsclient.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.client.dsclient.dto.ClientDTO;
+import com.devsuperior.client.dsclient.entities.Client;
+import com.devsuperior.client.dsclient.repositories.ClientRepository;
 
 @Service
 public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+		Page<Client> clients = repository.findAll(pageRequest);
+		return clients.map(x -> new ClientDTO(x));
+		
+		
+	}
+	
 	
 	
 
