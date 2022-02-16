6a437d3ca3825466f484820698f8c5cb745a4b20
OrlandoBG <orlandopsn1@gmail.com>
2022-02-16 16:49:15 -0300

    Encontrar client por id

--- src/main/java/com/devsuperior/client/dsclient/resources/ClientResources.java
+++ src/main/java/com/devsuperior/client/dsclient/resources/ClientResources.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,12 @@ import com.devsuperior.client.dsclient.dto.ClientDTO;
 import com.devsuperior.client.dsclient.services.ClientService;
 
 @RestController
+@RequestMapping(value = "/clients")
 public class ClientResources {
 	
 	@Autowired
 	private ClientService service;
 	
-	@RequestMapping(value = "/clients")
 	
 	public ResponseEntity<Page<ClientDTO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
@@ -30,9 +32,13 @@ public class ClientResources {
 		PageRequest pageRequest = PageRequest.of(page,linesPerPage,Direction.valueOf(direction),orderBy);
 		Page<ClientDTO> dto = service.findAllPaged(pageRequest);
 		
-		return ResponseEntity.ok().body(dto);
-		
-		
+		return ResponseEntity.ok().body(dto);	
 	}
-
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
 }
--- src/main/java/com/devsuperior/client/dsclient/services/ClientService.java
+++ src/main/java/com/devsuperior/client/dsclient/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.client.dsclient.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -19,9 +21,14 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
 		Page<Client> clients = repository.findAll(pageRequest);
-		return clients.map(x -> new ClientDTO(x));
-		
-		
+		return clients.map(x -> new ClientDTO(x));	
+	}
+	
+	@Transactional
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client client = obj.get();
+		return new ClientDTO(client);	
 	}
 	
 	
